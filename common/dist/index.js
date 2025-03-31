b6644c985d62c6c9026c9d097cec11f77b691fdc
Rakesh Bhagat <rakeshbhagat333@gmail.com>
2025-03-31 18:21:50 +0530

    added common type

---
+++ common/dist/index.js
@@ -0,0 +1,19 @@
+import z from "zod";
+export const signupInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(5),
+    name: z.string().optional()
+});
+export const signinInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(5)
+});
+export const createBlog = z.object({
+    title: z.string(),
+    content: z.string()
+});
+export const updateBlog = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.string()
+});
